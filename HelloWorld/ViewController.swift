00204b722e0c46cff8b07b10ba24520e7cbb38c8
IdleJatt <asktojatt@gmail.com>
2020-12-08 02:38:08 -0800

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Harpreet Singh on 08/12/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
